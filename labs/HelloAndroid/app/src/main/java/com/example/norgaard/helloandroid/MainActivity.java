851487b67e6fa5a120697f624945f481347c6b35
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-30 14:17:08 +0200

    hello world app done

---
+++ labs/HelloAndroid/app/src/main/java/com/example/norgaard/helloandroid/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.norgaard.helloandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void HelloClicked(View view) {
+        TextView derp = (TextView) findViewById(R.id.textView);
+
+        String helloAndroidText = "Hello Android";
+        String helloWorldText = "Hello World!";
+        if (derp.getText() != helloAndroidText) {
+            derp.setText(helloAndroidText);
+        } else {
+            derp.setText(helloWorldText);
+        }
+    }
+
+    public void ExitClicked(View view) {
+        System.exit(1);
+    }
+}
