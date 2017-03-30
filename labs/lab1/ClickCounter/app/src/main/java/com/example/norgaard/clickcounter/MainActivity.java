d4f7e67789fbcb414413716ae8dc84d06d20c04d
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-30 16:59:21 +0200

    click counter now working

--- labs/lab1/ClickCounter/app/src/main/java/com/example/norgaard/clickcounter/MainActivity.java
+++ labs/lab1/ClickCounter/app/src/main/java/com/example/norgaard/clickcounter/MainActivity.java
@@ -8,8 +8,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    int counter = 0;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +22,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        tv = (TextView) findViewById(R.id.counter);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                counter++;
+                tv.setText(counter + "");
             }
         });
     }
