3afdcd42e5eb4eca77212036a284ec942f0a0cb1
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-04-06 13:23:27 +0200

    created new project for selfie swapper and made persistance work for isSwapped state

---
+++ labs/lab2/SelfieSwap/app/src/main/java/com/example/norgaard/selfieswap/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.norgaard.selfieswap;
+
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.Console;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String SWAPPED_KEY = "asifdhsiudfkisduhfkiu";
+    boolean isSwapped;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (savedInstanceState != null) {
+            isSwapped = savedInstanceState.getBoolean(SWAPPED_KEY);
+            Log.d("Lifecycle", "onCreate(): isSwapped is : " + isSwapped);
+        }
+
+        setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.swapButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onClickSwapButton();
+            }
+        });
+    }
+
+    public void onClickSwapButton() {
+        if (isSwapped == true) {
+            isSwapped = false;
+        } else {
+            isSwapped = true;
+        }
+        Log.d("Button Click", "User clicked swap button, " + isSwapped);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+
+        outState.putBoolean(SWAPPED_KEY, isSwapped);
+
+        super.onSaveInstanceState(outState);
+    }
+}
