9f28e59390dfa6e3aafaec7e7d452f9789023aad
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-04-18 11:45:22 +0200

    created solution for post-it lab

---
+++ labs/lab3/Post-It/app/src/main/java/com/example/norgaard/post_it/ViewActivity.java
@@ -0,0 +1,13 @@
+package com.example.norgaard.post_it;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view);
+    }
+}
