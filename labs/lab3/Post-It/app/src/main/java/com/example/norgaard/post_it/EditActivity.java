2c78a177be5beb7fa38f010c5a162866f88946a6
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-04-18 11:55:55 +0200

    view activity now has button to start edit activity and the parent of edit is set to view

--- labs/lab3/Post-It/app/src/main/java/com/example/norgaard/post_it/EditActivity.java
+++ labs/lab3/Post-It/app/src/main/java/com/example/norgaard/post_it/EditActivity.java
@@ -1,7 +1,11 @@
 package com.example.norgaard.post_it;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EditActivity extends AppCompatActivity {
 
--- labs/lab3/Post-It/app/src/main/java/com/example/norgaard/post_it/ViewActivity.java
+++ labs/lab3/Post-It/app/src/main/java/com/example/norgaard/post_it/ViewActivity.java
@@ -1,13 +1,28 @@
 package com.example.norgaard.post_it;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewActivity extends AppCompatActivity {
+
+    Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view);
+
+        Button btnEdit = (Button) findViewById(R.id.btnStartEditActivity);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, EditActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
