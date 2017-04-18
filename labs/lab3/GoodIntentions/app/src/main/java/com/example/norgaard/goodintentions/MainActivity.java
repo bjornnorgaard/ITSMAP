3c0841e6d08d2ed3344738b4c1a494ba481b1a2a
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-04-18 20:11:23 +0200

    added click event handler for button

--- labs/lab3/GoodIntentions/app/src/main/java/com/example/norgaard/goodintentions/MainActivity.java
+++ labs/lab3/GoodIntentions/app/src/main/java/com/example/norgaard/goodintentions/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.norgaard.goodintentions;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnTakePicture ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnTakePicture = (Button) findViewById(R.id.btnTakePicture);
+        btnTakePicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TakePicture();
+            }
+        });
+    }
+
+    private void TakePicture() {
+        
     }
 }
