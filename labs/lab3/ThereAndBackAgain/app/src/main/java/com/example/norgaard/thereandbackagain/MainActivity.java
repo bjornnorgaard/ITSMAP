0e9d081b15b64c4d93ec70b63ceaa20a4e7042a2
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-04-18 11:21:48 +0200

    setting onclicklistener for button in main activity

--- labs/lab3/ThereAndBackAgain/app/src/main/java/com/example/norgaard/thereandbackagain/MainActivity.java
+++ labs/lab3/ThereAndBackAgain/app/src/main/java/com/example/norgaard/thereandbackagain/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.norgaard.thereandbackagain;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.btnGoToSecondActivity);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go to SecondActivity
+                Intent intent = new Intent(context, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
