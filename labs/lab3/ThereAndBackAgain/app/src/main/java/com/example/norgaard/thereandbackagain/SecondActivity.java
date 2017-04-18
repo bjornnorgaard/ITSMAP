d67d2dc95264a800ebfad6317677ac07b08018cc
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-04-18 11:30:58 +0200

    second activity now redirecting to main

--- labs/lab3/ThereAndBackAgain/app/src/main/java/com/example/norgaard/thereandbackagain/SecondActivity.java
+++ labs/lab3/ThereAndBackAgain/app/src/main/java/com/example/norgaard/thereandbackagain/SecondActivity.java
@@ -1,13 +1,28 @@
 package com.example.norgaard.thereandbackagain;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button btn = (Button) findViewById(R.id.btnReturnToMainActivity);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Go to SecondActivity
+                finish();
+            }
+        });
     }
 }
