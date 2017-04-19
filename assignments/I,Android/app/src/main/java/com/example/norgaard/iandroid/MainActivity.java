eda3c88cbe922685439bd7d8ea22047eecd4af16
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-04-19 19:47:31 +0200

    fab now calls EditProfile()

--- assignments/I,Android/app/src/main/java/com/example/norgaard/iandroid/MainActivity.java
+++ assignments/I,Android/app/src/main/java/com/example/norgaard/iandroid/MainActivity.java
@@ -21,11 +21,14 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                EditProfile();
             }
         });
+    }
+
+    private void EditProfile() {
+        
     }
 
     @Override
