d8bfd63932b4bbc6c552b1659673db85548f70f7
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-04-25 11:47:54 +0200

    Getting element onCreate()

--- labs/lab6/Allyourdatabasearebelongtous/app/src/main/java/com/example/norgaard/allyourdatabasearebelongtous/MainActivity.java
+++ labs/lab6/Allyourdatabasearebelongtous/app/src/main/java/com/example/norgaard/allyourdatabasearebelongtous/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.norgaard.allyourdatabasearebelongtous;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText taskPlaceEditText;
+    private EditText taskNameEditText;
+    private Button okButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        taskPlaceEditText = (EditText) findViewById(R.id.taskPlaceEditText);
+        taskNameEditText = (EditText) findViewById(R.id.taskNameEditText);
+        okButton = (Button) findViewById(R.id.okButton);
     }
 }
