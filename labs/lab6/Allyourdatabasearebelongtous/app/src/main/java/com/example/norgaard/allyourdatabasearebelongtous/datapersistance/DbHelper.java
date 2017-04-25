ba2417599a32becf288db1bbbf1c57cb101d7eac
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-04-25 12:04:18 +0200

    Started basic stuff for DB

---
+++ labs/lab6/Allyourdatabasearebelongtous/app/src/main/java/com/example/norgaard/allyourdatabasearebelongtous/datapersistance/DbHelper.java
@@ -0,0 +1,26 @@
+package com.example.norgaard.allyourdatabasearebelongtous.datapersistance;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.graphics.drawable.Drawable;
+
+public class DbHelper extends SQLiteOpenHelper {
+
+    static final int DATABASE_VERSION = 1;
+    static final String DATABASE_NAME = "task.db";
+
+    public DbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ labs/lab6/Allyourdatabasearebelongtous/app/src/main/java/com/example/norgaard/allyourdatabasearebelongtous/datapersistance/TaskContract.java
@@ -0,0 +1,10 @@
+package com.example.norgaard.allyourdatabasearebelongtous.datapersistance;
+
+import android.content.Context;
+
+public class TaskContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private TaskContract() {
+    }
+}
