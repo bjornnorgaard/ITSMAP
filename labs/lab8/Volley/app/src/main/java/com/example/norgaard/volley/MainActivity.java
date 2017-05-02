615f8cb62eba536eecff5762bd05b33e44d4f54f
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-02 12:00:18 +0200

    Made clicklisteren for button

--- labs/lab8/Volley/app/src/main/java/com/example/norgaard/volley/MainActivity.java
+++ labs/lab8/Volley/app/src/main/java/com/example/norgaard/volley/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.norgaard.volley;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView weatherTextView;
+    private Button getButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weatherTextView = (TextView) findViewById(R.id.weatherTextView);
+        getButton = (Button) findViewById(R.id.getButton);
+
+        getButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
