01a0eb420c397649912d12a3acd7a57caa42d09b
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-02 11:32:46 +0200

    Setup of view and main for getting weather

--- labs/lab8/JSONBourne/app/src/main/java/com/example/norgaard/jsonbourne/MainActivity.java
+++ labs/lab8/JSONBourne/app/src/main/java/com/example/norgaard/jsonbourne/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.norgaard.jsonbourne;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView weatherTextView;
+    private Button getWeatherButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWeatherButton = (Button) findViewById(R.id.getWeatherButton);
+        weatherTextView = (TextView) findViewById(R.id.weatherTextView);
+
+        getWeatherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getWeather();
+            }
+        });
+    }
+
+    private void getWeather() {
+        
     }
 }
