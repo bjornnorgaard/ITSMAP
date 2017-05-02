6e36b2f098eb32cc914123b480829939f0361071
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-02 11:55:13 +0200

    Wip

--- labs/lab8/JSONBourne/app/src/main/java/com/example/norgaard/jsonbourne/MainActivity.java
+++ labs/lab8/JSONBourne/app/src/main/java/com/example/norgaard/jsonbourne/MainActivity.java
@@ -6,6 +6,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView weatherTextView;
@@ -28,6 +35,37 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getWeather() {
-        
+        String urlString = "www.stuff.dk";
+        HttpsURLConnection connection = null;
+        InputStream stream = null;
+        String result = null;
+
+        try {
+            URL url = new URL(urlString);
+            connection = (HttpsURLConnection) url.openConnection();
+            connection.setReadTimeout(3000);
+            connection.setConnectTimeout(3000);
+            connection.setRequestMethod("GET");
+            connection.setDoInput(true);
+            connection.connect();
+
+            int responseCode = connection.getResponseCode();
+
+            if (responseCode != HttpsURLConnection.HTTP_OK) {
+                throw new IOException("HTTP error code" + responseCode);
+            }
+
+            stream = connection.getInputStream();
+
+            if (stream != null) {
+                result = readStream(stream,500);
+            }
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
