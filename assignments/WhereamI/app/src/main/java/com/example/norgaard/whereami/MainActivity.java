f6a4ac1fff93bbbd4c57c3da977b03f126c6961b
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-05-04 14:28:31 +0200

    Now using butterknife

--- assignments/WhereamI/app/src/main/java/com/example/norgaard/whereami/MainActivity.java
+++ assignments/WhereamI/app/src/main/java/com/example/norgaard/whereami/MainActivity.java
@@ -2,11 +2,26 @@ package com.example.norgaard.whereami;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.locationTextView)
+    private TextView locationTextView;
+
+    @BindView(R.id.startButton)
+    private Button startButton;
+
+    @BindView(R.id.stopButton)
+    private Button stopButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        ButterKnife.bind(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
